e362e3141a985b3e93489b50695a11be5c0a3e80
EthanZmr <289956508@qq.com>
2023-11-15 19:52:36 +0800

    io 模块新增

---
+++ netty/src/main/java/com/ethan/App.java
@@ -0,0 +1,11 @@
+package com.ethan;
+
+/**
+ * @author ethan
+ * @since 2023/11/6
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Netty");
+    }
+}
